8092a9797462a8d73ab5661d6feea61eb68e7a0f
Tassio Virginio <tassiovirginio@gmail.com>
2025-12-30 11:30:11 -0300

    update version

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@ description = "try-rs: Temporary workspace manager with TUI";
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "try-rs";
-          version = "0.1.30";  # atualize quando lançar nova
+          version = "0.1.31";  # atualize quando lançar nova
 
           src = self;  # usa o próprio repo como source
 
